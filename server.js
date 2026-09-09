1f6db9b2b8a7bc899767ba30de3b4212d5f6a539
believermutambo9-hub <believermutambo9@gmail.com>
2026-09-09 17:48:43 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { GoogleGenAI } from "@google/genai";
 const memory = new Map();
-
+const interactions = new Map();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
